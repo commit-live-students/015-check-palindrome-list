d0e7ab09d6777ee65aa1dabe859f34688f3b4070
Rohan2015 <rohandamodar@outlook.com>
2017-05-30 01:31:16 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
-def solution(list):
-    '''
-    Enter your code here
-    '''
-    return value
+def solution(user_list):
+    pal_list = []
+
+    for i in range(len(user_list)-1,-1,-1):
+                pal_list.append(user_list[i])
+    if pal_list == user_list:
+        return True
+    else:
+        return False
