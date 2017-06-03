1b436d4331311386ec2e323d7072302dcc0f4c8d
varunpanicker <varunpanicker@users.noreply.github.com>
2017-06-03 12:00:13 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
-def solution(list):
-    '''
-    Enter your code here
-    '''
-    return value
+def solution(l):
+    reverse = list(reversed(l))
+    if l==reverse:
+        print "Palindrome"
+        return True
+    else:
+        print "not palindrome"
+        return False
+solution([1,2,3,2,1])
