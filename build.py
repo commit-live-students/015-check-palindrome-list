01725ea0cd075d83ae249afd333c63fd53564d6f
bhattbhavesh91 <bhattbhavesh91@gmail.com>
2017-06-16 07:15:39 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
-def solution(list):
+def solution(list1):
     '''
     Enter your code here
     '''
+    if list1 == list1[::-1]:
+        value = True
+    else:
+        value = False
     return value
