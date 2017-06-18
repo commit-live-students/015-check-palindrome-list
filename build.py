aeeeb7c00daff2c835fc8538da96f0fad4bc5e8c
adinathauti <adinathauti@gmail.com>
2017-06-18 14:41:04 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,15 @@
-def solution(list):
-    '''
-    Enter your code here
-    '''
-    return value
+def solution(in_list):
+    in_list  = list(in_list)
+    cmp = list(in_list)
+    in_list.reverse()
+    print("Original list gets reversed ",in_list)
+    print("Copy of list is ",cmp)
+    isPal = True
+    for i in range(0,len(in_list)):
+        if(in_list[i]!=cmp[i]):
+            isPal = False
+            break
+    print isPal
+    return isPal
+
+solution('NITI')
