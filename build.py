223e31f9d80e11276e4c44275b87c5d0b08992af
PankajMehar <pankaj02mehar@gmail.com>
2017-07-10 05:59:06 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,11 @@
-def solution(list):
-    '''
-    Enter your code here
-    '''
-    return value
+
+def solution(my_list):
+    if len(my_list) <= 1:
+        return True
+    else:
+        if my_list[0] == my_list[-1]:
+            return solution(my_list[1:-1])
+        else:
+            return False
+my_list=['n','i','t','i','n']
+solution(my_list)
