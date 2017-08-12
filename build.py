04742cb2752cd06adfeb9b7541dede7b5c6f73e9
SiddharthPant <spsiddharthpant@gmail.com>
2017-08-12 08:07:34 +0000

    Done

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
-def solution(list):
-    '''
-    Enter your code here
-    '''
+def solution(a_list):
+    k = list(a_list)
+    k.reverse()
+    value = True if k == a_list else False
     return value
