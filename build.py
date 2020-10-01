31b56cf4042d9125ecf5ce7d35e3741f3bec1dc4
SHRIKANT ASHOK DANDGE <69064261+SHRISHRIKANT@users.noreply.github.com>
2020-10-01 09:33:20 +0530

    Rewrite build.py

--- build.py
+++ build.py
@@ -1,5 +1,28 @@
 def solution(list):
-    '''
-    Enter your code here
-    '''
-    return value
+    # creating an empty list 
+    lst = [] 
+    
+    # number of elemetns as input 
+    n = int(input("Enter number of elements : ")) 
+    
+    # iterating till the range 
+    for i in range(0, n): 
+        ele = input()
+    
+        lst.append(ele) # adding the element
+
+    out_list = lst[::-1] #reversing a list
+    m=0
+    #check if palindrome
+    for i in range(0,n):
+        if lst[i]==out_list[i]:
+            m=m+1
+        else:
+            print("FALSE")
+            break
+    if m==n:
+        print("TRUE")
+            
+
+ solution(list)
+        
