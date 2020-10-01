e34294e1c972e2730a3b23e6598351ad33468cff
prikshitkuhar16 <72150244+prikshitkuhar16@users.noreply.github.com>
2020-10-01 20:25:25 +0530

    Rewrite build.py

--- build.py
+++ build.py
@@ -1,5 +1,16 @@
 def solution(list):
-    '''
-    Enter your code here
-    '''
+    if list == list[::-1]:
+        value = "Yes it is a palindrome list"
+    else:
+        value = "No it is not a palindrome list"
     return value
+
+
+if __name__ == " __main__":
+    n = input("Enter how many items you want in your list --> ")
+    input_list = []
+    for i in range(int(n)):
+        list_items = input(f"Enter item {i+1} of list --> ")
+        input_list.append(list_items)
+
+    print(solution(input_list))
